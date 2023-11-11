e81e8452b0b03cf45d85b062a7bcfce85dc893f1
Antinotmanist <katya.goryacheva.2009@gmail.com>
2023-11-11 10:26:40 +1000

    Создание простого класса и объекта

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello Worldo')
+class Animal:
+    name = None
+    age = None
+    gender = None
+
+    def __init__(self, name='', age=0, gender=False):
+        self.name = str(name)
+        self.age = int(age)
+        self.gender = bool(gender)
+
+
+dog = Animal(name='chapi', age=2, gender=1)
+print('name:',dog.name)
+print('age:',dog.age)
+print('gender:',dog.gender)
