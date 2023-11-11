7cec143e1d8b0972e8ae649c8228b2ccbc931163
Antinotmanist <katya.goryacheva.2009@gmail.com>
2023-11-11 15:41:56 +1000

    Добавление __init__

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    def __init__(self, name: str, price: float, quantity=0):
+        # assert делает утверждение которое должно выполняться
+        assert price >= 0, f'Ваша цена {price} не входит в нужные условия'
+        assert quantity >= 0, f'идиот'
 
-item1 = Item()
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
 
-item2 = Item()
-item2.name = 'BebraLop'
-item2.price = 1000
-item2.quantity = 3
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-print(item2.calculate_total_price(item2.price, item2.quantity))
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    
+item1 = Item('Phone', 100, 5)
+
+item2 = Item('BebraLop', 1000, 3)
+
+print(item1.calculate_total_price())
