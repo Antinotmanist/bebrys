c673fe036b6bab8737896c4ae38062d317e942cf
Antinotmanist <katya.goryacheva.2009@gmail.com>
2023-11-11 18:32:45 +1000

    Add __repr__

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Item:
+    pay_rate = 0.8
+    all = []
+
     def __init__(self, name: str, price: float, quantity=0):
         # assert делает утверждение которое должно выполняться
         assert price >= 0, f'Ваша цена {price} не входит в нужные условия'
@@ -9,12 +12,23 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
     
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f'Item("{self.name}", {self.price}, {self.quantity})'
     
-item1 = Item('Phone', 100, 5)
+    
+item1 = Item('Phone', 100, 1)
+item2 = Item('Laptop', 1000, 3)
+item3 = Item('Cable', 10, 5)
+item4 = Item('Mouse', 50, 5)
+item5 = Item('Keyboard', 75, 5)
 
-item2 = Item('BebraLop', 1000, 3)
-
-print(item1.calculate_total_price())
+print(Item.all)
